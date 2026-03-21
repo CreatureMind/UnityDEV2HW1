1f6240b45cbd38411aa33d7b5269d3bdf3f75c34
abb2k <75845401+abb2k@users.noreply.github.com>
2026-03-22 01:56:01 +0200

    only one character shwing :D

---
+++ Assets/Scripts/Managers/SpecificCharacterEnabler.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SpecificCharacterEnabler : MonoBehaviour
+{
+    [SerializeField] private GameObject walkingCharacter;
+    [SerializeField] private GameObject dancingCharacter;
+
+    void Awake()
+    {
+        ArrowsSpawner.OnTrackStarted += TrackStarted;
+        InGameUI.OnMenuClosed += TrackEnded;
+
+        TrackEnded();
+    }
+
+    void TrackStarted()
+    {
+        walkingCharacter.SetActive(false);
+        dancingCharacter.SetActive(true);
+    }
+
+    void TrackEnded()
+    {
+        walkingCharacter.SetActive(true);
+        dancingCharacter.SetActive(false);
+    }
+}
